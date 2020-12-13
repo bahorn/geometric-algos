dd02698d995ef601fb00cf394ebbc195c4843cce
casper <casperlamboo@gmail.com>
2020-12-14 00:37:49 +0100

    gloss rendering

--- app/Main.hs
+++ app/Main.hs
@@ -1,10 +1,12 @@
 module Main where
 
 import MiscUtils.Generation (randomInput)
+import Display.Display
 import Display.ConvexHull
 import ConvexHull.Algorithms.QuickHull
-import ConvexHull.Algorithms.GrahamScan 
+import ConvexHull.Algorithms.GrahamScan
 import ConvexHull.Algorithms.JarvisMarch
+import qualified Graphics.Gloss as Gloss
 
 main :: IO ()
 main = do
@@ -15,4 +17,10 @@ main = do
     let hull3 = jarvisMarch points
     print $ hull1 == hull2
     print hull3
-    plotHull "test.png" points (hull3)-- ++ [head hull3])
+    Gloss.display
+      (Gloss.InWindow "Geometric Algorithms" (512, 512) (10, 10))
+      Gloss.white
+      (Gloss.pictures
+        [ Gloss.color (Gloss.greyN 0.7) . render $ hull1
+        , render points
+        ])
--- src/Display/Display.hs
+++ src/Display/Display.hs
@@ -1,1 +1,13 @@
-module Display.Display where
+module Display.Display (Display (..)) where
+
+import Data.Maybe
+import qualified Graphics.Gloss as Gloss
+
+class Display a where
+  render :: a -> Gloss.Picture
+
+instance Display a => Display [a] where
+  render = Gloss.pictures . map render
+
+instance Display a => Display (Maybe a) where
+  render = maybe Gloss.Blank render
